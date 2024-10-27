4882f64250f2d696c08e424df39995fc493c9b3d
Pasha Shchukin <pasha4320692@gmail.com>
2024-10-27 19:13:36 +0200

    make build

---
+++ index.bb3c7aa0.js
@@ -0,0 +1,2 @@
+Array.from(document.querySelector("table").querySelectorAll("tr")).forEach(function(e){var r=e.querySelectorAll("td, th");if(r.length>2){var t=r[1].cloneNode(!0);e.insertBefore(t,r[r.length-1])}});
+//# sourceMappingURL=index.bb3c7aa0.js.map
