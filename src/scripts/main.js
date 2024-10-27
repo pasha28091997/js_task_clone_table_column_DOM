0715272cfc724d1edd8ae09eeb9fdb6cab148da4
Pasha Shchukin <pasha4320692@gmail.com>
2024-10-27 19:05:00 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rows = table.querySelector('tr');
+
+rows.forEach((row) => {
+  const cells = row.querySelector('td, th');
+
+  if (cells.length > 2) {
+    const clonedCells = cells[1].cloneNode(true);
+
+    row.inserBefore(clonedCells, cells[cells.length - 1]);
+  }
+});
